d73f7264086b4be4fee69dce03cd6f611977f257
Artur Caliendo Prado <artur.prado@gmail.com>
2021-01-28 09:01:36 -0300

    Create about page (#1)

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/home';
-// import About from './pages/about';
+import About from './pages/about/about';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/about" component={About} /> */}
+      <Route path="/about" component={About} />
     </Switch>
   </BrowserRouter>
 );
