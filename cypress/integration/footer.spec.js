f206b13fac9030c0c2a045f19dd549e9199ed033
Artur Caliendo Prado <artur.prado@gmail.com>
2021-02-06 10:54:36 -0300

    Test application with cypress

---
+++ cypress/integration/footer.spec.js
@@ -0,0 +1,16 @@
+describe('Header', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/characters', { fixture: 'characters.json' })
+    cy.visit('/')
+  })
+
+  it('contains a footer', () => {
+    cy.get('footer')
+      .should('have.length', 1)
+  })
+
+  it('contains copyright information', () => {
+    cy.get('#site-footer')
+      .contains('All rights reserved. © Star Track Corp, 2021.')
+  })
+});
---
+++ cypress/integration/header.spec.js
@@ -0,0 +1,40 @@
+describe('Header', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/characters', { fixture: 'characters.json' })
+    cy.visit('/')
+  })
+
+  it('contains the name of the site', () => {
+    cy.get('#top-header .description a')
+      .contains('Star Track')
+  })
+
+  it('contains a link to the home page in the title', () => {
+    cy.get('#top-header .description a')
+      .should('have.attr', 'href')
+      .and('include', '/')
+  })
+
+  it('contains a link to the home page in the logo', () => {
+    cy.get('#top-header a').first()
+      .should('have.attr', 'href')
+      .and('include', '/')
+  })
+
+  it('contains a logo', () => {
+    cy.get('#top-header img.logo')
+      .should('have.length', 1)
+  })
+
+  it('contains a subtitle', () => {
+    cy.get('#top-header .description label')
+      .first()
+      .should('have.text', 'Use your knowledge to help Dr. Ken')
+  })
+
+  it('contains a link to the ranking', () => {
+    cy.get('#top-header .ranking-container a')
+      .should('have.attr', 'href')
+      .and('include', '/ranking')
+  })
+});
