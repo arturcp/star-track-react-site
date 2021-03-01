813957d12fe5348516923954f8737a1d552e550f
Artur Caliendo Prado <artur.prado@gmail.com>
2021-03-01 15:21:05 -0300

    Create state machine and use it to improve the tutorial page (#19)

---
+++ src/libs/StateMachine.js
@@ -0,0 +1,117 @@
+// A state machine is a behavior model. It consists of a
+// finite number of states and is therefore also called
+// finite-state machine (FSM).
+//
+// This class cannot be used directly as it is not exported.
+// The reason is that it expects a very specific format
+// in the data it is built upon, so, instead of instantiating
+// the class developers can use the `createStateMachine` function
+// below.
+//
+// An example of the resulting data:
+//
+//   data = {
+//     mainComponent: [object],
+//     initial: 'green',
+//     states: {
+//       green: { next: 'yellow', component: fn },
+//       yellow: { next: 'red', component: fn },
+//       red: { next: '', component: fn },
+//   }
+//
+// But it is not necessary to build it by hand. The
+// `createStateMachine` expects these parameters:
+//
+// * Component: the component that will make use of the
+//   state machine. Usually, it will receive `this`.
+//
+// * states: this is an array of strings with the
+//   states in the correct order. Example:
+//   ['green', 'yellow', 'red'].
+//
+// By creating a state machine into a component, it will
+// automatically gain two methods:
+//
+// * nextStage: moves to the next stage of the state machine and
+//   returns the hash that contains the name of the next stage and
+//   the function to build the component.
+//
+// * componentForCurrentStage: this function builds the
+//   component to be rendered. It will call a method
+//   named on<current stage name>, and the current stage
+//   name must be in camel case, starting with a capital
+//   letter. These functions must be defined in the original
+//   component.
+//
+//   Example: if the current stage is `introduction`, it will
+//   call a function named onIntroduction. if this function does
+//   not exist, it will return null;
+//
+// Also, a variable will be added to the component's state
+// called `currentStage`.
+class StateMachine {
+  constructor(data) {
+    this.data = data;
+    this.currentState = data.initial;
+  }
+
+  current = () => ({
+    value: this.currentState,
+    ...this.data.states[this.currentState]
+  })
+
+  next = () => {
+    this.currentState = this.current().next
+    return this.current()
+  }
+
+  component = () => {
+    const current = this.current()
+    if (current && current.component && typeof current.component === 'function') {
+      return current.component(this.data.mainComponent)
+    } else {
+      return null;
+    }
+  }
+}
+
+export const createStateMachine = (Component, states) => {
+  const data = {
+    mainComponent: Component,
+    initial: states[0],
+    states: {}
+  }
+
+  const capitalize = (s) => {
+    if (typeof s !== 'string') {
+      return ''
+    }
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
+  for (let i = 0; i < states.length - 1; i++) {
+    let currentState = states[i],
+        nextState = states[i+1];
+
+    data.states[currentState] = {
+      next: nextState,
+      component: Component[`on${capitalize(currentState)}`]
+    }
+  }
+
+  const lastState = states[states.length - 1];
+  data.states[lastState] = { next: '', component: Component[`on${capitalize(lastState)}`] }
+
+  const stateMachine = new StateMachine(data);
+
+  Component.nextStage = () => {
+    Component.setState({ currentStage: stateMachine.next().value })
+  }
+
+  Component.componentForCurrentStage = () => (
+    stateMachine.component()
+  )
+
+  Component.stateMachine = stateMachine;
+  Component.setState({ currentStage: states[0] })
+};
--- src/pages/tutorial/tutorial.js
+++ src/pages/tutorial/tutorial.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import Introduction from './introduction';
 import ConfirmationBox from '../../components/ConfirmationBox/ConfirmationBox';
 import Dialogs from '../../components/Dialogs/Dialogs';
+import { createStateMachine } from '../../libs/StateMachine';
 import rocket from '../../images/rocket-launch.gif';
 
 import './styles.scss';
@@ -12,70 +13,65 @@ import './styles.scss';
 // the state property of the <Link> component that
 // redirected the user to this page.
 export default class Tutorial extends Component {
-  STAGES = {
-    INTRODUCTION: 'introduction',
-    CONFIRMATION: 'confirmation',
-    GAME: 'game',
-    LAUNCHING: 'launching',
-  }
-
-  state = {
-    character: this.props.location.state.character,
-    stage: this.STAGES.INTRODUCTION
-  }
-
-  onDialogFinish = () => {
-    this.setState({ stage: this.STAGES.CONFIRMATION })
-  }
-
-  startGame = () => {
-    this.setState({ stage: this.STAGES.LAUNCHING })
-  }
-
-  tutorialStage = () => {
-    const { stage, character } = this.state
-
-    if (stage === this.STAGES.INTRODUCTION) {
-      return (
-        <Introduction
-          character={character}
-          dialogFinished={this.onDialogFinish}
-        />
-      )
-    } else if (stage === this.STAGES.CONFIRMATION) {
-      const text = `You are about to start ${character.name}'s journey, are you ready?`
-
-      return (
-        <ConfirmationBox
-          title="It starts"
-          text={text}
-          buttonText="Start"
-          onClickHandler={this.startGame}
-        />
-      )
-    } else if (stage === this.STAGES.LAUNCHING) {
-      setTimeout(() => {
-        this.setState({ stage: this.STAGES.GAME })
-      }, 5000);
-      return (
-        <img
-          className="rocket-launch"
-          src={rocket}
-          alt="Rocket launch"
-        />
-      )
-    } else {
-      return (
-        <Dialogs character={this.state.character} />
-      );
+  constructor(props) {
+    super(props)
+    this.state = {
+      character: this.props.location.state.character
     }
+
+    createStateMachine(this,
+      ['introduction', 'confirmation', 'launching', 'dialogs']);
+  }
+
+  onIntroduction = () => {
+    return (
+      <Introduction
+        character={this.state.character}
+        dialogFinished={this.nextStage}
+      />
+    )
+  }
+
+  onConfirmation = () => {
+    const text = `You are about to start ${this.state.character.name}'s journey, are you ready?`
+
+    return (
+      <ConfirmationBox
+        title="It starts"
+        text={text}
+        buttonText="Start"
+        onClickHandler={this.nextStage}
+      />
+    )
+  }
+
+  onLaunching = () => {
+    setTimeout(() => {
+      if (this.state.currentStage === 'launching') {
+        this.nextStage()
+      }
+    }, 5000);
+
+    return (
+      <img
+        className="rocket-launch"
+        src={rocket}
+        alt="Rocket launch"
+      />
+    )
+  }
+
+  onDialogs = () => {
+    return (
+      <Dialogs character={this.state.character} />
+    );
   }
 
   render() {
     return (
       <section id="tutorial">
         <Header />
-        {this.tutorialStage()}
+        {this.componentForCurrentStage()}
       </section>
     )
   }
