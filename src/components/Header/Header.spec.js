edf0c64dc177610ab57dd207eae1f47583eb9ab3
Artur Caliendo Prado <artur.prado@gmail.com>
2021-03-06 11:31:28 -0300

    Test header and footer using jest (#22)

---
+++ src/components/Header/Header.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+
+describe('Header UI', () => {
+  it('should match the snapshot', () => {
+    const component = renderer.create(
+      <Router>
+        <Header />
+      </Router>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+});
