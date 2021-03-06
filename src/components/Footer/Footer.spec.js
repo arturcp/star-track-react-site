9655f9f3093b6e74e7c9d8d3e4d621b76f65f5a6
Artur Caliendo Prado <artur.prado@gmail.com>
2021-03-06 11:30:48 -0300

    Test header and footer using jest

---
+++ src/components/Footer/Footer.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './Footer';
+
+describe('Footer UI', () => {
+  it('should match the snapshot', () => {
+    const component = renderer.create(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+});
